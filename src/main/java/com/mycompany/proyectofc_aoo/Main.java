cc04e96bd16aeff845796d6d73f5dd4e3208ea2c
ArielPalmaLaz <apalma8824@utm.edu.ec>
2024-01-25 17:07:39 -0500

    Finalización del analisis de tiempos

--- src/main/java/com/mycompany/proyectofc_aoo/Main.java
+++ src/main/java/com/mycompany/proyectofc_aoo/Main.java
@@ -6,45 +6,58 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
-        int op, M;
-        String[][] _a = null;
-        Matriz matriz = new Matriz(); // Creación de una instancia de la clase Matriz para realizar operaciones en matrices.
-        Scanner sc = new Scanner(System.in); // Objeto Scanner para leer la entrada del usuario.
+        int op = 0, M = 0; //2ta
+        String[][] _a = null; //ta
+        Matriz matriz = new Matriz(); // ta
+        Scanner sc = new Scanner(System.in); //ta
         do {
             // Menú de opciones para el usuario.
             System.out.println("1.-Insertar Valor 'M' que tendra la matriz MxM");
             System.out.println("2.-Mostrar patron A");
             System.out.println("3.- Mostrar patron B");
             System.out.println("4.-Salir");
-            op = sc.nextInt(); // Captura de la opción seleccionada por el usuario.
+            op = sc.nextInt(); // W+ta
             switch (op){
-                case 1:
+                // A = Numero de veces que se repite el caso 1
+                case 1: //
                     do {
+                        //x = Veces que se repite el "do" del caso 1
                         System.out.println("Inserte el valor de M");
-                        M = sc.nextInt(); // Captura del tamaño 'M' de la matriz, asegurándose de que sea al menos 1.
-                    } while (M < 1);
-                    _a = new String[M][M]; // Creación de una nueva matriz cuadrada de tamaño MxM.
-                    matriz.llenarMatriz(_a); // Llamada al método llenarMatriz para ingresar valores en la matriz.
-                    System.out.println("Matriz "+M+"x"+M+" ingresada");
+                        M = sc.nextInt(); // WA(x+ta)
+                    } while (M < 1); // WA(x + tc) 
+                    _a = new String[M][M]; // WAta 
+                    matriz.llenarMatriz(_a); // WA(5ta + (n+1)(ta+2tc) + F(2ta + 2tc + to + 2Cta + Cto + Ctc) + tc) 
+                    System.out.println("Matriz "+M+"x"+M+" ingresada"); //WA(2to)
                     break;
                 case 2:
-                    if (_a == null) {
+                    //B = Numero de veces que se repite el caso 2
+                    if (_a == null) { // WBtc
                         System.out.println("No existe ninguna matriz");
                     } else {
-                        matriz.mostrarA(_a); // Llamada al método mostrarA para mostrar el patrón A en la matriz.
+                        matriz.mostrarA(_a); // WB[2ta + tc + F(ta+to+tc) + [F(F+1)/2](3ta+3tc+3to)].
                     }
                     break;
                 case 3:
-                    if (_a == null) {
+                    // C = Numero de veces que se repite el caso 3 
+                    if (_a == null) { //WCtc
                         System.out.println("No existe ninguna matriz");
                     } else {
-                        matriz.mostrarB(_a); // Llamada al método mostrarB para mostrar el patrón B en la matriz.
+                        matriz.mostrarB(_a); // WC{ta + F(3ta+2to+2tc) + [F(F+1)/2]*[(2ta+2to+2tc)}
                     }
                     break;
                 default:
-                    System.out.println(op == 4 ? "Saliendo del Programa..." : "No existe la opcion seleccionada");
+                    // D = Numero de veces que se repite el caso default
+                    System.out.println(op == 4 ? "Saliendo del Programa..." : "No existe la opcion seleccionada"); // WDtc
             }
-        } while (op != 4); // El bucle continúa hasta que el usuario elige la opción 4 para salir del programa.
+        } while (op != 4); // W = Número de veces que se repite el menu.            W+tc
         
+        /*
+            Tm = 2ta + ta + ta + ta + ta + tc 
+            
+            Tp = 5ta + W{ta+A[x+ta+x+tc+ta+ 5ta + (n+1)(ta+2tc) 
+                + F(2ta + 2tc + to + 2Cta + Cto + Ctc) + tc) +2to] 
+                + B[tc+2+ta+tc+ F(ta+to+tc) + (F(F+1)/2](3ta+3tc+3to))]
+                + C[ta+F(3ta+2to+2tc) + (F(F+1)/2)(2ta+to+2tc)]} + 1 + tc
+        */ 
     }
 }
