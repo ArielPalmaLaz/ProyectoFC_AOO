4a934cf25374fe8787c706d8dea09d958dd28171
ArielPalmaLaz <apalma8824@utm.edu.ec>
2024-01-24 20:14:25 -0500

    1.-Se agrega lo hecho en el Proyecto de Medio Ciclo 2.-Se comenta el primer y segundo método para poder hallar los tiempos estimados de compilacion.

--- src/main/java/com/mycompany/proyectofc_aoo/Main.java
+++ src/main/java/com/mycompany/proyectofc_aoo/Main.java
@@ -1,20 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.mycompany.proyectofc_aoo;
 
-/**
- *
- * @author ArielPalma
- */
-public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+import java.util.Scanner;
+
+public class Main {
     
+    public static void main(String[] args) {
+        int op, M;
+        String[][] _a = null;
+        Matriz matriz = new Matriz(); // Creación de una instancia de la clase Matriz para realizar operaciones en matrices.
+        Scanner sc = new Scanner(System.in); // Objeto Scanner para leer la entrada del usuario.
+        do {
+            // Menú de opciones para el usuario.
+            System.out.println("1.-Insertar Valor 'M' que tendra la matriz MxM");
+            System.out.println("2.-Mostrar patron A");
+            System.out.println("3.- Mostrar patron B");
+            System.out.println("4.-Salir");
+            op = sc.nextInt(); // Captura de la opción seleccionada por el usuario.
+            switch (op){
+                case 1:
+                    do {
+                        System.out.println("Inserte el valor de M");
+                        M = sc.nextInt(); // Captura del tamaño 'M' de la matriz, asegurándose de que sea al menos 1.
+                    } while (M < 1);
+                    _a = new String[M][M]; // Creación de una nueva matriz cuadrada de tamaño MxM.
+                    matriz.llenarMatriz(_a); // Llamada al método llenarMatriz para ingresar valores en la matriz.
+                    System.out.println("Matriz "+M+"x"+M+" ingresada");
+                    break;
+                case 2:
+                    if (_a == null) {
+                        System.out.println("No existe ninguna matriz");
+                    } else {
+                        matriz.mostrarA(_a); // Llamada al método mostrarA para mostrar el patrón A en la matriz.
+                    }
+                    break;
+                case 3:
+                    if (_a == null) {
+                        System.out.println("No existe ninguna matriz");
+                    } else {
+                        matriz.mostrarB(_a); // Llamada al método mostrarB para mostrar el patrón B en la matriz.
+                    }
+                    break;
+                default:
+                    System.out.println(op == 4 ? "Saliendo del Programa..." : "No existe la opcion seleccionada");
+            }
+        } while (op != 4); // El bucle continúa hasta que el usuario elige la opción 4 para salir del programa.
+    }
 }
---
+++ src/main/java/com/mycompany/proyectofc_aoo/Matriz.java
@@ -0,0 +1,67 @@
+
+package com.mycompany.proyectofc_aoo;
+
+import java.util.Scanner;
+
+public class Matriz {
+    
+    // Constructor vacío de la clase Matriz.
+    public Matriz() {
+    }
+    
+    // Método para llenar la matriz con el símbolo dado.
+    public String[][] llenarMatriz(String a[][]) {
+        //USAREMOS 2 VARIABLES, F = número de filas, C = Número de columnas
+        Scanner sc = new Scanner(System.in); //ta
+        String str; //ta
+        int filas = a.length;  //ta 
+        int columnas = a[0].length;//ta 
+        do {
+            System.out.println("Elija con que caracter se llenara la matriz"); 
+            str = sc.next(); //ta 
+        } while (!this.comprobacionLetra(String.valueOf(str))); //n*tc
+        for (int i = 0; i < filas; i++) { //F(tc+to+ta)+ta
+            for (int j = 0; j < columnas; j++) { //F*C*(tc+to+ta)+ta
+                a[i][j] = str; // F*C*(ta+to)
+            }
+        }
+        /* ta + ta + ta + ta + ta + ntc + Ftc + Fto + Fta + ta + FCtc + FCto + FCta + ta+FCta + FCto 
+           6ta +ntc + m(tc+to+ta+Ctc+Cto+Cta+Cta+Cto)
+           6ta + ntc + F[C(2ta+2to+tc)+tc+to+ta] 
+           TIEMPO ESTIMADO = 7ta + to + tc + ntc + F[C[2ta+2to+tc)] 
+        */
+        return a; // Devuelve la matriz llena.
+    }
+
+    private boolean comprobacionLetra(String str) {
+        if (str.length() != 1) {  //tc
+            System.out.println("Porfavor ingrese tan solo un caracter");  
+            return false; 
+        } else {
+            return true;
+        }
+        //TIEMPO ESTIMADO = tc
+    }
+    // Método para mostrar el patrón A en la matriz.
+    public void mostrarA(String a[][]) {
+        int filas = a.length;  //ta
+        for (int i = 0; i < filas; i++) {  //
+            for (int j = 0; j <= i; j++) {
+                System.out.print(a[i][j] + " "); // Muestra cada posición de la matriz según el patrón A.
+            }
+            System.out.println(""); // Salto de línea al final de cada fila.
+        }
+    }
+
+    // Método para mostrar el patrón B en la matriz.
+    public void mostrarB(String a[][]) {
+        int controlador = a.length;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < controlador; j++) {
+                System.out.print(a[i][j] + " "); // Muestra cada posición de la matriz según el patrón B.
+            }
+            controlador--; // Controla la cantidad de elementos a mostrar en cada fila.
+            System.out.println(""); // Salto de línea al final de cada fila.
+        }
+    }
+}
